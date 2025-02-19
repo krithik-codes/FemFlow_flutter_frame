576dd7839190e604c231d4310ba7117751024753
krithik-codes <krithikbm7@gmail.com>
2025-02-19 16:58:16 +0530

    base file recreation

---
+++ flowfem_app/lib/pages/intro2.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Intro2 extends StatelessWidget {
+  const Intro2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
