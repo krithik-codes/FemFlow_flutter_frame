0720d26a0ac594b76f54be147313f34d4fbf97cd
krithik-codes <krithikbm7@gmail.com>
2025-02-20 05:43:27 +0530

    final_without_polish

--- flowfem_app/lib/main.dart
+++ flowfem_app/lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        fontFamily: 'Poppins',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: Intro1(),
